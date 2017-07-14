d7c6255fd88d13ba171e5bf1b2552d983700cd86
borrom <borrom.dev@gmail.com>
2017-07-14 14:59:03 +0700

    setup

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec rails server --port 3000
+angular: ng build --watch
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
-def index
-  render: file => 'public/index.html'
+  def index
+    render :file => "public/index.html"
+  end
 end
