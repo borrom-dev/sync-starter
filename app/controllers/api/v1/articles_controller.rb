647ac6f2d3dfb1b947d4b00478dbc2a1acf8658c
borrom <borrom.dev@gmail.com>
2017-07-14 15:48:38 +0700

    add articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,42 @@
-class V1::ArticlesController < Api::RestController
+class Api::V1::ArticlesController < Api::RestController
+  before_action :set_article, only: [:show, :update, :destroy]
+
   def index
-    render :json=> {greeting: 'Hello, World'}
+    @articles = Article.all
+    render json: @articles
   end
+
+  def show
+    render json: @article
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body, :published)
+    end
 end
